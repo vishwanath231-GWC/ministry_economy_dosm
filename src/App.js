dd30381fb4b5b07916f3de16d3e83a3c7cbfc612
vishwanath231-GWC <vishwanath.a@gwcdata.ai>
2024-07-29 18:13:25 +0530

    setup

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import Chart from "react-apexcharts";
+
+const App = () => {
+
+  const [options] = useState({
+    chart: {
+      id: "basic-bar",
+    },
+    xaxis: {
+      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+    },
+  });
+
+  const [series] = useState([
+    {
+      name: "series-1",
+      data: [30, 40, 45, 50, 49, 60, 70, 91],
+    },
+  ]);
+
+  return (
+    <div>
+      <h2>Hello world!</h2>
+      <Chart options={options} series={series} type="bar" width="500" />
+    </div>
+  );
+};
+
+export default App;
